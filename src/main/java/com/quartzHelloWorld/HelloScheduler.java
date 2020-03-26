7739b16a7c51bc3c8ebcddf109a2079ef9855536
gates008 <89143998@qq.com>
2020-03-26 23:37:38 +0800

    init

--- src/main/java/com/quartzHelloWorld/HelloScheduler.java
+++ src/main/java/com/quartzHelloWorld/HelloScheduler.java
@@ -16,20 +16,37 @@ import org.quartz.impl.StdSchedulerFactory;
 public class HelloScheduler {
 
 	public static void main(String[] args) throws SchedulerException {
+		Date startDate = new Date();
+		startDate.setTime(startDate.getTime()+3000);
+		Date endDate = new Date();
+		endDate.setTime(endDate.getTime()+6000);
+		
 		//创建一个JobDetail实例，将该实例与HelloJob Class 绑定
 		JobDetail jobDetail = (JobDetail) JobBuilder.newJob(HelloWorldJob.class).
-				withIdentity("HelloJob", "group1").build();
+				withIdentity("HelloJob", "group1").usingJobData("msg", "msg")
+				.usingJobData("num", 3.14F).build();
 		//创建一个Trigger实例，定义该job立即执行，并且每隔两秒种重复执行一次，直到永远。
 		Trigger trigger = TriggerBuilder.newTrigger().withIdentity("myTrigger", "group1")
+				.usingJobData("msg", "trigger")
+				.usingJobData("num", 3.15D)
 				.startNow().withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInMilliseconds(1000).
 						repeatForever()).build();
+		//设置开始结束时间
+//		Trigger trigger1 = TriggerBuilder.newTrigger().withIdentity("myTrigger", "group1")
+//				.startAt(startDate).endAt(endDate)
+//				.usingJobData("msg", "trigger")
+//				.usingJobData("num", 3.15D)
+//				.withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInMilliseconds(1000).
+//						repeatForever()).build();
 		//创建Schedule实例
 		SchedulerFactory sfact = new StdSchedulerFactory();
 		Scheduler scheduler = sfact.getScheduler();
 		scheduler.start();
 		Date date = new Date();
 		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		//输出当前时间
 		System.out.println("Current time is :"+sf.format(date));
+		//调用任务计划执行Job
 		scheduler.scheduleJob(jobDetail,trigger);
 	}
 
--- src/main/java/com/quartzHelloWorld/HelloWorldJob.java
+++ src/main/java/com/quartzHelloWorld/HelloWorldJob.java
@@ -4,16 +4,52 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
+import org.quartz.TriggerKey;
 
 public class HelloWorldJob implements Job{
 	
+	private String msg;
+	private Double num;
 	@Override
-	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		//HellWorld 执行
 		Date date = new Date();
 		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		System.out.println(sf.format(date)+"----hello Test---");
+		
+		//withIdentity获取方式
+		JobKey key = context.getJobDetail().getKey();
+		System.out.println(sf.format(date)+"---"+key.getName()+"---"+key.getGroup());
+		TriggerKey tkey = context.getTrigger().getKey();
+		System.out.println(sf.format(date)+"---"+tkey.getName()+"---"+tkey.getGroup());
+		//JobDataMap 获取方式一
+		JobDataMap job_dataMap = context.getJobDetail().getJobDataMap();
+		JobDataMap trigger_DataMap1 = context.getTrigger().getJobDataMap();
+		String jobmsg = job_dataMap.getString("msg");
+        Float jobnum = job_dataMap.getFloat("num");
+        String tribmsg = trigger_DataMap1.getString("msg");
+        Double trinum = trigger_DataMap1.getDouble("num");
+        System.out.println("JobDataMap: "+jobmsg +" " +jobnum);
+        System.out.println("TriDataMap: "+tribmsg +" " +trinum);
+        //JobDataMap 获取方式二 如果key相同以trigger为主
+        JobDataMap job_dataMap1 = context.getMergedJobDataMap();
+        String tribmsg1 = job_dataMap1.getString("msg");
+        Double trinum1 = job_dataMap1.getDouble("num");
+        System.out.println("TriDataMap1: "+tribmsg1 +" " +trinum1);
+        //JobDataMap 获取方式三 get/set方式  如果key相同以trigger为主
+        System.out.println("TriDataMap2: "+msg +" " +num);
+        //设置开始结束时间打印
+//        System.out.println("开始时间："+context.getTrigger().getStartTime() +
+//        		"结束时间：" +context.getTrigger().getEndTime());
 	}
-
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public void setNum(Double num) {
+		this.num = num;
+	}
 }
